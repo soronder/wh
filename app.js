7ab78801ecd5d372da604d92e2e5c6f895fad411
soronder <81733515+soronder@users.noreply.github.com>
2021-02-13 01:00:35 -0600

    fully working mongo

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var fs = require('fs');
+// var fs = require('fs');
+
+const { MongoClient } = require('mongodb');
 
 var uuid = require('uuid');
-var uniq = require('lodash.uniq');
-var without = require('lodash.without');
+// var uniq = require('lodash.uniq');
+// var without = require('lodash.without');
+var keyBy = require('lodash.keyby');
 
 const dirTree = require('directory-tree');
 // const imageThumbnail = require('image-thumbnail');
@@ -22,37 +25,37 @@ const shared = {
     stateChange: false,
 };
 
-try {
-    const filePath = path.join(__dirname, 'state-backup.json');
-    if (fs.existsSync(filePath)) {
-        shared.state = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    }
-    else {
-        shared.state = {};
-    }
-}
-catch(e) {
-    console.log('Error initializing state from file');
-    throw e;
-}
+// try {
+//     const filePath = path.join(__dirname, 'state-backup.json');
+//     if (fs.existsSync(filePath)) {
+//         shared.state = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+//     }
+//     else {
+//         shared.state = {};
+//     }
+// }
+// catch(e) {
+//     console.log('Error initializing state from file');
+//     throw e;
+// }
 
-setInterval(function() {
-    if(!shared.stateChange) {
-        return;
-    }
-    var start = new Date();
-    var hrstart = process.hrtime();
+// setInterval(function() {
+//     if(!shared.stateChange) {
+//         return;
+//     }
+//     var start = new Date();
+//     var hrstart = process.hrtime();
 
-    fs.writeFileSync(path.join(__dirname, 'state.json'), JSON.stringify(shared.state));
+//     fs.writeFileSync(path.join(__dirname, 'state.json'), JSON.stringify(shared.state));
 
-    var end = new Date() - start,
-        hrend = process.hrtime(hrstart);
+//     var end = new Date() - start,
+//         hrend = process.hrtime(hrstart);
 
-    console.info('Write execution time: %dms', end);
-    console.info('Write execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
+//     console.info('Write execution time: %dms', end);
+//     console.info('Write execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
 
-    shared.stateChange = false;
-}, 1000);
+//     shared.stateChange = false;
+// }, 1000);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -61,65 +64,88 @@ app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/static', express.static(path.join(__dirname, 'client/build/static')));
 
-// app.use('/', indexRouter);
 
-app.get('/', function(req, res) {
-    return res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
+const uri = "garbage mongo URL";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/dir', function(req, res) {
-    const tree = dirTree('public/images');
-    return res.json(tree);
-});
+client.connect(async (err) => {
+    const collection = client.db("whouse").collection("items");
 
-app.get('/meta', function(req, res) {
-    const path = req.query && req.query.path;
-    const auth = getAuth(req);
-    if(auth && path) {
-        let meta = shared.state[path] || {
-            description: '',
-            comments: []
-        };
-        return res.json(meta);
-    }
-    return res.json(shared.state);
-});
+    // app.use('/', indexRouter);
 
-app.post('/meta', function(req, res) {
-    const path = req.query && req.query.path;
-    const auth = getAuth(req);
-    if(auth && path) {
-        let meta = shared.state[path];
-        if(!meta) {
-            meta = shared.state[path] = {
-                description: '',
-                comments: []
-            }
-        }
-        if(req.body.comment) {
-            meta.comments.push({
-                author: auth.name,
-                time: new Date().getTime(),
-                comment: req.body.comment,
+    app.get('/', function(req, res) {
+        return res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    });
+
+    app.get('/dir', function(req, res) {
+        const tree = dirTree('public/images');
+        return res.json(tree);
+    });
+
+    app.get('/meta', function(req, res) {
+        const path = req.query && req.query.path;
+        const auth = getAuth(req);
+        if(auth && path) {
+            return collection.findOne({path}).then((result) => {
+                return res.json(result || {
+                    description: '',
+                    comments: []
+                });
             });
         }
-        if(req.body.description) {
-            meta.description = req.body.description;
-        }
-        if(req.body.claimed !== undefined) {
-            meta.claimed = meta.claimed || [];
-            if(req.body.claimed) {
-                meta.claimed.push(auth.name);
-                meta.claimed = uniq(meta.claimed);
+        return collection.find().toArray().then((results) => {
+            let meta = keyBy(results, 'path')
+            return res.json(meta);
+        });
+    });
+
+    app.post('/meta', function(req, res) {
+        const path = req.query && req.query.path;
+        const auth = getAuth(req);
+        if(auth && path) {
+            let value = { $set: { path } };
+            if(req.body.comment) {
+                value.$push = {
+                    comments: {
+                        author: auth.name,
+                        time: new Date().getTime(),
+                        comment: req.body.comment,
+                    }
+                };
             }
-            else {
-                meta.claimed = without(meta.claimed, auth.name);
+            if(req.body.description) {
+                value.$set.description = req.body.description;
             }
+            if(req.body.claimed !== undefined) {
+                if(req.body.claimed) {
+                    value.$addToSet = {
+                        claimed: auth.name
+                    };
+                }
+                else {
+                    value.$pull = { claimed: auth.name };
+                }
+            }
+            return collection.findOneAndUpdate({ path }, value, { upsert: true } )
+                .then(result => {
+                    if(result.ok === 1) {
+                        return collection.findOne({path}).then((result) => {
+                            return res.json(result);
+                        });
+                    }
+                    return res.json({ error: 'failed to update' });
+                });
         }
-        shared.stateChange = true;
-        return res.json(meta);
-    }
-    return null;
+        return null;
+    });
+
+    app.get('/thumb', function(req, res) {
+
+    });
+
+    app.use('/users', usersRouter);
+
+    // client.close();
 });
 
 function getAuth(req) {
@@ -152,11 +178,5 @@ app.post('/login', function(req, res) {
     }
     return res.json({error: "Invalid Login"});
 });
-
-app.get('/thumb', function(req, res) {
-
-});
-
-app.use('/users', usersRouter);
 
 module.exports = app;
